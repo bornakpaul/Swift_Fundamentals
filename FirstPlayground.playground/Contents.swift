560056963d976f3488770e20916e0fd6e7207667
Bornak <bornakpaul@gmail.com>
2021-12-23 01:23:41 +0530

    changes

--- FirstPlayground.playground/Contents.swift
+++ FirstPlayground.playground/Contents.swift
@@ -1,7 +1,22 @@
 
+// when we have an initial value, we don't need to type the annotation value.
 
 var playerName = "Bornak"
 var age = 21
 var temperature = 72.6
 var activeMembership = true
+
+
+
+// Suppose we will use the variable latter and we don't have to initialize a value from the beginning
+// that's when we will declare the type of the variable like below :
+
+//   : think of this colon as "of type"
+//   ( : Int ) this together is called "type annotation"
+
+var bonusScore: Int
+var environmentName: String
+var levelCompleted: Bool
+var progressPercentage: Double
+
 
