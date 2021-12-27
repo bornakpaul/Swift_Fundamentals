fd05ff0ea3f65f38a6aa7d6c6858c0bbb4c7c9d4
Bornak <bornakpaul@gmail.com>
2021-12-27 21:52:25 +0530

    Dictionary

--- SixthPlayground.playground/Contents.swift
+++ SixthPlayground.playground/Contents.swift
@@ -26,3 +26,63 @@ var second = Movie(title: "Sing Street", director: "John Carney", releaseYear: 2
 
 print(first.summary())
 print(second.summary())
+
+
+
+//
+//
+//
+// Dictionaries
+//
+//
+// in other languages it's also know as MAP, Symbol Table , Associative Array or Dictionary
+//
+//
+//
+// Dictionary is multiple key and value pair
+
+var airlines = ["SWA": "Southwest Airline",
+                "BAW": "British Airways",
+                "BHA": "Buddha Air",
+                "CPA": "Cathay Pacific"]
+
+// use [ ] to look up a key
+//let result = airlines["SWA"]
+//print(result)
+
+// but the above structure gives us a optional value as the value requested may not be there in the first place
+// so we use the below structure
+
+if let results = airlines["SWA"]{
+    print(results)
+}else{
+    print("No match found")
+}
+
+// ADD OR CHANGE
+airlines["DVA"] = "Discovery Airlines"  // this will add a new key/value
+airlines
+
+// remove by setting to nil
+airlines["BHA"] = nil
+airlines
+
+for (code, airline) in airlines {
+    print(code,airline)
+}
+
+for entry in airlines{
+    print(entry)
+}
+
+
+
+
+
+// Dictionary of String keys and String values
+var periodicElements: [String: String]
+
+// Dictionary of Int keys and String values
+var employees: [Int: String]
+
+
