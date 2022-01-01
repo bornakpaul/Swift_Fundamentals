93a3a8cedf9880174579adf4a11d32aee2f7b7f1
Bornak <bornakpaul@gmail.com>
2022-01-01 21:02:13 +0530

    initialiser and de-initializer

--- NinthPlayground.playground/Contents.swift
+++ NinthPlayground.playground/Contents.swift
@@ -1,11 +1,34 @@
 // Using Classes and Objects in Swift
 
+import CoreGraphics
+
 class Appliance {
     // properties
-    var manufacturer: String = ""
-    var model: String = ""
-    var voltage: Int = 0
+    var manufacturer: String
+    var model: String
+    var voltage: Int
     var capacity: Int?
+    
+    // initializer
+    init(){
+        self.manufacturer = "default manufacturer"
+        self.model = "default model"
+        self.voltage = 120
+    }
+    
+    // additional initializer
+    init(withVoltage: Int){
+        self.manufacturer = "default manufacturer"
+        self.model = "default model"
+        self.voltage = withVoltage
+    }
+    
+    // deinitializer
+    deinit{
+        // perform cleanup code here ...
+        // release a file resource ...
+        // release a network resource ...
+    }
     
     // methods
     func getDetails() -> String {
@@ -16,6 +39,12 @@ class Appliance {
         return message
     }
 }
+
+var cafetiere = Appliance(withVoltage: 220)
+cafetiere.manufacturer = "Megappliance EU"
+cafetiere.model = "Electrotiere"
+print(cafetiere.getDetails())
+
 
 // ...later, create an instance of appliance
 var kettle = Appliance()
