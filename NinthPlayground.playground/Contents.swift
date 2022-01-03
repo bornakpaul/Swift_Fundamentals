a3061ee6c26c7afce7b35ee623673dda062b2026
Bornak <bornakpaul@gmail.com>
2022-01-03 21:09:20 +0530

    new

--- NinthPlayground.playground/Contents.swift
+++ NinthPlayground.playground/Contents.swift
@@ -52,3 +52,47 @@ kettle.manufacturer = "Meggappliance Inc"
 kettle.model = "TeaMaster 5000"
 kettle.voltage = 220
 print(kettle.getDetails())
+
+// ... new example
+
+class Singer {
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+    
+    func sing(){
+        print("I am bornak")
+    }
+}
+
+// this colon means extends
+// CounterSinger extends Singer
+//
+class CounterSinger: Singer{
+    override func sing(){
+        print("I will definetly learn iOS and be a iOS Developer.")
+    }
+}
+
+class HeavyMetalSinger: Singer{
+    var noiceLevel: Int
+    
+    init(name: String, age: Int, noiceLevel: Int) {
+        self.noiceLevel = noiceLevel
+        super.init(name: name, age: age)
+    }
+    
+    override func sing() {
+        print("Grrrr rargh rargh rarrrgh!")
+    }
+}
+
+//var taylor = Singer(name: "Bornak", age: 21)
+var taylor = CounterSinger(name: "Taylor", age: 30)
+taylor.name
+taylor.age
+taylor.sing()
